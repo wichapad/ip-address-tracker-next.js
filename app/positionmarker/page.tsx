942da0017468957d6531eb01aa58227add6eb4a9
guy <guy_wichapad@hotmail.com>
2024-01-11 11:35:12 +0700

    fix code

--- app/positionmarker/page.tsx
+++ app/positionmarker/page.tsx
@@ -10,28 +10,31 @@ interface LocationData {
 interface MarkerProps {
   data: LocationData;
 }
-export default function Postionmarker({ data }: MarkerProps) {
-  const position: [number, number] = useMemo(() => {
-    return [data.latitude, data.longitude];
-  }, [data.latitude, data.longitude]);
-  const map = useMap();
 
-  useEffect(() => {
-    map.flyTo(position, 13, {
-      animate: true,
-    });
-  }, [map, position]);
-  return (
-    <Marker
-      icon={L.icon({
-        iconUrl: "/icon-location.svg",
-        iconSize: [32, 40],
-        iconAnchor: [10, 41],
-        popupAnchor: [2, -40],
-      })}
-      position={position}
-    >
-      <Popup>This is the location of the IP address or Domain</Popup>
-    </Marker>
-  );
+const PostionMarker = ({ data }:MarkerProps) => {
+    const position: [number, number] = useMemo(() => {
+        return [data.latitude, data.longitude];
+      }, [data.latitude, data.longitude]);
+      const map = useMap();
+    
+      useEffect(() => {
+        map.flyTo(position, 13, {
+          animate: true,
+        });
+      }, [map, position]);
+      return (
+        <Marker
+          icon={L.icon({
+            iconUrl: "/icon-location.svg",
+            iconSize: [32, 40],
+            iconAnchor: [10, 41],
+            popupAnchor: [2, -40],
+          })}
+          position={position}
+        >
+          <Popup>This is the location of the IP address or Domain</Popup>
+        </Marker>
+      );
 }
+
+export default PostionMarker
